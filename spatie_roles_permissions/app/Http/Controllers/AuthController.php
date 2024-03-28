9ac3837f228a69e795b2f1310cb578f5ea1f5fac
warrenshiv <warrenshiv@gmail.com>
2024-03-28 09:44:19 +0300

    Add AuthController and RegisterRequest classes

---
+++ spatie_roles_permissions/app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\RegisterRequest;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $request->validated();
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'role_id' => $request->role_id,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+}
---
+++ spatie_roles_permissions/app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'max:255'],
+            'email' => ['required', 'email', 'unique:users'],
+            'role_id' => ['required'],
+            'password' => ['required', 'min:8'],
+            'password_confirmed' => ['required', 'same:password']
+        ];
+    }
+}
