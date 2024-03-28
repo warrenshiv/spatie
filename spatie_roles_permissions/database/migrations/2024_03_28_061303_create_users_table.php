7b926b7401aa4555c4e39c23082024d8703a35ec
warrenshiv <warrenshiv@gmail.com>
2024-03-28 09:24:09 +0300

    Add Role and User Controllers and Models

--- spatie_roles_permissions/database/migrations/2024_03_28_061303_create_users_table.php
+++ spatie_roles_permissions/database/migrations/2024_03_28_061303_create_users_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
---
+++ spatie_roles_permissions/database/seeders/RoleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::insert([
+            ['name' => 'Super Admin'],
+            ['name' => 'Author'],
+            ['name' => 'Editor']
+        ]);
+    }
+}
